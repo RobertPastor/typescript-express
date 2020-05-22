419b04e9aa8583f82e34ec2530f7f06b28a35b23
Robert <robert.pastor0691@orange.fr>
2020-05-22 12:16:44 +0200

    sonar cloud request authentication OK
    sonar cloud request authentication OK

--- src/test/requestTests.test.ts
+++ src/test/requestTests.test.ts
@@ -47,41 +47,95 @@ test('test request to sonar cloud', done => {
             log(JSON.stringify(res.headers))
             if (res.statusCode === 200) {
                 log(JSON.stringify(body));
-
-                url = "https://sonarcloud.io/api/webservices/list";
-                let optionsTwo = {
-                    uri: url,
-                    method: 'GET',
-                    headers: {
-                        'Content-Type': 'application/json',
-                        'Accept': 'application/json',
-                        'Accept-Charset': 'utf-8',
-                        "auth": {
-                            'user': username,
-                            "pass": token
-                        }
-                    }
-                };
-
-                request(optionsTwo, (err: any, res: Response, body: any) => {
-                    if (err) {
-                        log(err)
-                        expect(json === undefined).toBe(true);
-                        done(err);
-                    } else {
-
-                        //log(JSON.stringify(res))
-                        if (res.statusCode === 200) {
-                            log(JSON.stringify(body));
-                        }
-                    }
-                    expect(body === undefined).toBe(false);
-                    done()
-                })
-
             }
+            done();
         }
     });
 
 })
 
+
+test('test request to sonar cloud', done => {
+
+    let username: string = "RobertPastor";
+    let token: string = "c410aa98569fd6a31f67473c4c6a941246ec8fa8";
+
+    let url = "https://sonarcloud.io/api/webservices/list";
+    let optionsTwo = {
+        uri: url,
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            'Accept-Charset': 'utf-8',
+            "auth": {
+                'user': username,
+                "pass": token
+            }
+        }
+    };
+    let json: any = undefined
+    request(optionsTwo, (err: any, res: Response, body: any) => {
+        if (err) {
+            log(err);
+            expect(json === undefined).toBe(true);
+
+            done(err);
+        } else {
+
+            log(JSON.stringify(res.statusCode))
+            log(JSON.stringify(res.headers))
+            if (res.statusCode === 200) {
+                //log(JSON.stringify(body));
+            }
+            expect(body === undefined).toBe(false);
+            done()
+        }
+
+    })
+}, 10000)
+
+test('test request to sonar cloud', done => {
+
+    request.debug = true;
+    /**
+     * curl.exe -v -u c410aa98569fd6a31f67473c4c6a941246ec8fa8: https://sonarcloud.io/api/user_tokens/search?login=RobertPastor@github
+     */
+
+    let username: string = "RobertPastor";
+    let token: string = "c410aa98569fd6a31f67473c4c6a941246ec8fa8";
+    let auth: string = "Basic " + Buffer.from(token + ":").toString("base64");
+
+    let url = "https://sonarcloud.io/api/webservices/list";
+    url = "https://sonarcloud.io/api/user_tokens/search?login=RobertPastor@github";
+    url = "https://sonarcloud.io/api/projects/search?p=1&ps=100&organization=robertpastor";
+    let optionsTwo = {
+        uri: url,
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            'Accept-Charset': 'utf-8',
+            "Authorization": auth
+        }
+    };
+    let json: any = undefined
+    request(optionsTwo, (err: any, res: Response, body: any) => {
+        if (err) {
+            log(err);
+            expect(json === undefined).toBe(true);
+
+            done(err);
+        } else {
+
+            log(JSON.stringify(res.statusCode))
+            log(JSON.stringify(res.headers))
+            if (res.statusCode === 200) {
+                log(JSON.stringify(body));
+            }
+            expect(body === undefined).toBe(false);
+            done()
+        }
+
+    })
+}, 10000)
